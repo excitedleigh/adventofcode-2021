5d136edacae0df37e46c526e80c13619b9c734f1
Leigh Brenecki <l@leigh.net.au>
2021-12-01 21:00:27 +1100

    Day 1

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use itertools::Itertools;
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    day1a();
+}
+
+fn day1() {
+    let stdin = io::stdin();
+    {
+        let handle = stdin.lock();
+        let count = handle
+            .lines()
+            .map(|x| x.unwrap().parse::<i32>().unwrap())
+            .tuple_windows()
+            .filter(|(a, b)| a < b)
+            .count();
+        println!("count: {}", count);
+    }
+}
+
+fn day1a() {
+    let stdin = io::stdin();
+    {
+        let handle = stdin.lock();
+        let count = handle
+            .lines()
+            .map(|x| x.unwrap().parse::<i32>().unwrap())
+            .tuple_windows()
+            .map(|(a, b, c)| a + b + c)
+            .tuple_windows()
+            .filter(|(a, b)| a < b)
+            .count();
+        println!("count: {}", count);
+    }
+}
