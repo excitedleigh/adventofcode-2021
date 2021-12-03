f1a0819f0b9a2d23edd73e786da79bfb608bdd30
Leigh Brenecki <l@leigh.net.au>
2021-12-03 21:16:02 +1100

    Day 3

---
+++ src/day3.rs
@@ -0,0 +1,128 @@
+use std::io;
+use std::io::BufRead;
+
+pub(crate) fn day3() {
+    let stdin = io::stdin();
+    let stdin_lock = stdin.lock();
+    let bit_count = stdin_lock
+        .lines()
+        .map(|x| {
+            x.unwrap()
+                .chars()
+                .map(|c| match c {
+                    '1' => 1,
+                    '0' => -1,
+                    a => panic!("{:?} is not binary", a),
+                })
+                .collect::<Vec<_>>()
+        })
+        .fold(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], |acc, val| {
+            acc.iter().zip(val.iter()).map(|(a, b)| a + b).collect()
+        });
+    println!("bit_count={:?}", bit_count);
+    let mut gamma = 0;
+    let mut epsilon = 0;
+    for bit in bit_count {
+        gamma = gamma << 1;
+        epsilon = epsilon << 1;
+        if bit > 0 {
+            gamma += 1;
+        } else if bit < 0 {
+            epsilon += 1;
+        } else {
+            panic!("one of the bits was exactly zero");
+        }
+    }
+    println!("{:?}", gamma * epsilon);
+}
+
+#[derive(Debug)]
+struct Candidate {
+    val: u32,
+    is_o2_candidate: bool,
+    is_co2_candidate: bool,
+}
+
+pub(crate) fn day3a() {
+    /*
+    Different tack for this one, converting the values to numbers early and using bitwise ops instead of string manipulation.
+
+    This does not work.
+    */
+    let stdin = io::stdin();
+    let mut line_list: Vec<_> = {
+        stdin
+            .lock()
+            .lines()
+            .map(|x| Candidate {
+                val: u32::from_str_radix(x.unwrap().as_str(), 2).unwrap(),
+                is_o2_candidate: true,
+                is_co2_candidate: true,
+            })
+            .collect()
+    };
+    for n in (0..11).rev() {
+        let bit_mask = 1 << n;
+        let o2_most_commonly_set = is_most_commonly_set(
+            line_list
+                .iter()
+                .filter(|x| x.is_o2_candidate)
+                .map(|x| x.val),
+            bit_mask,
+        );
+        line_list
+            .iter_mut()
+            .filter(|x| x.is_o2_candidate)
+            .for_each(|x| {
+                if ((x.val & bit_mask) != 0) != o2_most_commonly_set {
+                    x.is_o2_candidate = false
+                }
+            });
+    }
+    for n in (0..11).rev() {
+        let bit_mask = 1 << n;
+        let co2_most_commonly_set = is_most_commonly_set(
+            line_list
+                .iter()
+                .filter(|x| x.is_co2_candidate)
+                .map(|x| x.val),
+            bit_mask,
+        );
+        line_list
+            .iter_mut()
+            .filter(|x| x.is_co2_candidate)
+            .for_each(|x| {
+                if ((x.val & bit_mask) != 0) == co2_most_commonly_set {
+                    x.is_co2_candidate = false
+                }
+            });
+        // I'm not happy with a lot of this, but this line is the bit I'm least happy with.
+        if line_list.iter().filter(|x| x.is_co2_candidate).count() == 1 {
+            break;
+        }
+    }
+    println!("{:?}", line_list);
+    let o2 = only(line_list.iter().filter(|x| x.is_o2_candidate)).val;
+    let co2 = only(line_list.iter().filter(|x| x.is_co2_candidate)).val;
+    println!("o2={}, co2={}, product={}", o2, co2, o2 * co2);
+}
+
+fn is_most_commonly_set<V: Iterator<Item = u32>>(values: V, bit_mask: u32) -> bool {
+    let mut most_commonly_set: i32 = 0;
+    for value in values {
+        if (value & bit_mask) != 0 {
+            most_commonly_set += 1
+        } else {
+            most_commonly_set -= 1
+        }
+    }
+    return most_commonly_set >= 0;
+}
+
+fn only<T, I: Iterator<Item = T>>(mut iter: I) -> T {
+    let val = iter.next().expect("Iterator was empty");
+    if let Some(_) = iter.next() {
+        panic!("Iterator had more than one item");
+    }
+    val
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env::args;
 
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     match &args().nth(1).unwrap()[..] {
@@ -9,6 +10,8 @@ fn main() {
         "1a" => day1::day1a(),
         "2" => day2::day2(),
         "2a" => day2::day2a(),
+        "3" => day3::day3(),
+        "3a" => day3::day3a(),
         _ => panic!("that day hasn't been added yet"),
     }
 }
